9b1c04335499dfa54fa81f83e2d105eeb15303ca
Marius Heine <m.heine@geprog.com>
2025-09-19 00:59:33 +0200

    properly style slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -9,13 +9,13 @@
       }" />
     </div>
 
-    <div class="-mt-4.5">
+    <div class="-mt-[17px]">
       <input :value="props.selectedIndex" type="range" :min="0" :max="props.dateOptions.length - 1"
         class="custom-slider w-full relative z-10" @input="emit('update:selectedIndex', +$event.target.value)">
     </div>
 
     <div class="relative w-full h-5 mt-2">
-      <span v-for="group in props.dateGroup" :key="group.year" class="absolute text-sm text-white" :style="{
+      <span v-for="group in props.dateGroup" :key="group.year" class="absolute text-sm text-white whitespace-nowrap font-medium" :style="{
         left: `${parseFloat(group.offset) + parseFloat(group.width) / 2}%`,
         transform: 'translateX(-50%)',
         fontSize: props.isSmallScreen ? '0.50rem' : '1rem',
@@ -47,3 +47,45 @@ const emit = defineEmits<{
 
 const { t } = useI18n();
 </script>
+
+<style scoped>
+.custom-slider {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 100%;
+  height: 8px;
+  background: transparent;
+  position: relative;
+  z-index: 2;
+  cursor: pointer;
+}
+
+.custom-slider::-webkit-slider-runnable-track {
+  height: 4px;
+  background: transparent;
+  border-radius: 9999px;
+}
+
+.custom-slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: white;
+  border: 2px solid #0f172b;
+  cursor: pointer;
+  position: relative;
+  z-index: 10;
+  margin-top: -8px;
+}
+
+.custom-slider::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background: white;
+  border: 2px solid #0f172b;
+  cursor: pointer;
+}
+</style>
