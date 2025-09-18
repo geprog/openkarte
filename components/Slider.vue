d31452838efb212e23365d96e49d74c08864dad2
Marius Heine <m.heine@geprog.com>
2025-09-19 00:54:49 +0200

    properly align slider

--- components/Slider.vue
+++ components/Slider.vue
@@ -1,38 +1,25 @@
 <template>
-  <div class="absolute z-[100] bottom-0 left-1/2 transform -translate-x-1/2 w-full px-6 py-4 bg-slate-900 bg-opacity-80">
+  <div
+    class="absolute z-[100] bottom-0 left-1/2 transform -translate-x-1/2 w-full px-6 py-4 bg-slate-900 bg-opacity-80">
     <div class="relative w-full h-2 rounded overflow-hidden">
-      <div
-        v-for="group in props.dateGroup"
-        :key="group.year"
-        class="absolute h-full"
-        :style="{
-          left: `${group.offset}%`,
-          width: `${group.width}%`,
-          backgroundColor: group.color,
-        }"
-      />
+      <div v-for="group in props.dateGroup" :key="group.year" class="absolute h-full" :style="{
+        left: `${group.offset}%`,
+        width: `${group.width}%`,
+        backgroundColor: group.color,
+      }" />
     </div>
 
-    <input
-      :value="props.selectedIndex"
-      type="range"
-      :min="0"
-      :max="props.dateOptions.length - 1"
-      class="custom-slider w-full relative z-10"
-      @input="emit('update:selectedIndex', +$event.target.value)"
-    >
+    <div class="-mt-4.5">
+      <input :value="props.selectedIndex" type="range" :min="0" :max="props.dateOptions.length - 1"
+        class="custom-slider w-full relative z-10" @input="emit('update:selectedIndex', +$event.target.value)">
+    </div>
 
-    <div class="relative w-full h-5">
-      <span
-        v-for="group in props.dateGroup"
-        :key="group.year"
-        class="absolute text-sm text-white"
-        :style="{
-          left: `${parseFloat(group.offset) + parseFloat(group.width) / 2}%`,
-          transform: 'translateX(-50%)',
-          fontSize: props.isSmallScreen ? '0.50rem' : '1rem',
-        }"
-      >
+    <div class="relative w-full h-5 mt-2">
+      <span v-for="group in props.dateGroup" :key="group.year" class="absolute text-sm text-white" :style="{
+        left: `${parseFloat(group.offset) + parseFloat(group.width) / 2}%`,
+        transform: 'translateX(-50%)',
+        fontSize: props.isSmallScreen ? '0.50rem' : '1rem',
+      }">
         {{ group.year }}
       </span>
     </div>
